40c18d214be7c6c3ded958921a66e2f4c3f70d1f
Adhavan-km <123286658+Adhavan-km@users.noreply.github.com>
2025-01-25 00:33:49 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "“A winking emoji indicates someone is being mischievous or humorous. It shows that someone is joking, flirting, or has a hidden agenda. ",
+  },
+  {
+    id: 5,
+    emoji: "😛",
+    name: "Face With Stuck-Out Tongue",
+    meaning:
+      "“You make fun of others: I knew you'd screw up. The emoji grins cheekily with its tongue stuck out to playfully create a humorous mood or soften an ironic remark.",
+  },
+  {
+    id: 6,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "“I'm the king of the world! Smart and casual, this emoji features a face with a wide, closed smile and black sunglasses.",
+  },
+];
+
+export default emojipedia;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
